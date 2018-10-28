b2d3648b558cfd22f1adb84ca5e4b453fe016437
ovbarve <ovbarve@ncsu.edu>
2018-10-27 22:45:57 -0400

    added graphs folder and updated code and readme updated

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Regression_analysis
+
+
+The regression analysis was carried ou on this data: ovbarve.csv 
+The analysis was carried out according to this project task list.
+
+
+setup the environment
+
+
+install conda and setup env
+Download the installer:
+
+    Miniconda installer for Linux.
+
+
+```
+cd ~/Downloads
+bash Miniconda3-latest-Linux-x86_64.sh
+conda create --name name_of_env
+source activate name_of_env
+conda install matplotlib scipy pandas statsmodels seaborn pylab
+
+```
+
+Run the regression code 
+
+```
+python regression.py
+```
+
+The results and discussion can be found in the regression_project.pdf
+Also all the graphs can be found here.
--- regression.py
+++ regression.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import csv 
+import os
 import numpy as np
 import ast
 import pandas as pd
@@ -8,17 +9,33 @@ import statsmodels.api as sm
 from statsmodels.formula.api import ols
 from statsmodels.sandbox.regression.predstd import wls_prediction_std
 import matplotlib.pyplot as plt
+from sklearn import *
+from scipy.stats import chisquare
+import scipy.stats as stats
+from scipy import stats
+
 
 #task 1
 #read csv into pandas data frame
 df = pd.read_csv("ovbarve.csv")
+os.chdir("graphs")
 
 #function to plot histogram
 def plot_histogram(array, count):
-    plt.hist(array, bins=30)
-    plt.ylabel('Probability')
-    plt.title("Histogram :"+str(count))
-    plt.xlabel('Value bins')
+    if count == 5:
+        fig = plt.figure()
+        plt.hist(array, bins=30)
+        plt.ylabel('Probability')
+        plt.title("Histogram Y")
+        plt.xlabel('Dependent variable with bin size as 30')
+        fig.savefig("Histogram Y.png")
+    else:
+        fig = plt.figure()
+        plt.hist(array, bins=30)
+        plt.ylabel('Probability')
+        plt.xlabel('independent variables with bin size as 30')
+        plt.title("Histogram X:"+str(count + 1))
+        fig.savefig("Histogram X:"+str(count + 1)+".png")
     plt.show()
 
 #calculate mean and variance of each column and plot histogram
@@ -26,99 +43,169 @@ regression_variable = list(df.keys())
 for count,variable in enumerate(regression_variable):
     print("mean of regression_variable: x"+str(count+1), df[variable].mean())
     print("variance of regression_variable: x"+str(count+1), df[variable].var())
-    plot_histogram(df[variable], count)
+    #plot_histogram(df[variable], count)
+
 
 #plot correlation matrix
+#fig = plt.figure()
 print(df.corr())
 pd.scatter_matrix(df)
+plt.savefig("correlation_scatter_plot.png")
 plt.show()
 
-"""
-# remove outliers at 2 std deviations and outside
-elements = np.array(x1)
+# remove outliers at 3 std deviations and outside
+print(df.shape)
 
-mean = np.mean(elements, axis=0)
-sd = np.std(elements, axis=0)
-
-final_list = [x for x in x1 if (x > mean - 2 * sd)]
-final_list = [x for x in final_list if (x < mean + 2 * sd)]
-print(final_list)
-
-#plot correlation matrix
-df = pd.read_csv("ovbarve.csv")
-print(df.corr())
-pd.scatter_matrix(df)
-plt.show()
-"""
+df = df[(np.abs(stats.zscore(df)) < 3).all(axis=1)]
+print(df.shape)
+input()
 
 #Task 2
 #Linear regression
 
-regression_variables = [x1, x2, x3, x4, x5]
+regression_variables = list(df.keys())
+y = df[regression_variables[-1]]
 
-#reshape to fit for regression
-regression_variables = [[[each_feature] for each_feature in array] for array in regression_variables]
+for count,variable in enumerate(regression_variables[:-1]):
 
-for count,variable in enumerate(regression_variables):
-    fig = plt.figure()
-    X = sm.add_constant(variable) # adding a constant
+    #prepare data by reshaping vector eg. [1, 2, 3] to [[1], [2], [3]]    
+    current_independent_variable = [[i] for i in df[variable]]
+    
+    X = sm.add_constant(current_independent_variable) # adding a constant
     model = sm.OLS(y, X).fit()
     predictions = model.predict(X) 
     print_model = model.summary()
     print(print_model)
     
     #calculate residual array
-    residual =  y - predictions  
-    
-    #residual scatter plot 
-    plt.subplot(3, 1, 1)    
-    plt.scatter(variable, residual,color='g')
-    plt.ylabel('residual');
-    plt.title("Residual scatter plot :"+str(count))
-    plt.xlabel('x - variable')
-
-
-    #residual histogram
-    plt.subplot(3, 1, 2)
-    plt.hist(residual, bins=30)
-    plt.title("Histogram of residual:"+str(count))        
-
-    lm=linear_model.LinearRegression()
-    lm.fit(variable, y)
-
-    plt.subplot(3, 1, 3)
-    plt.scatter(variable, y,color='g')
-    plt.plot(variable, lm.predict(variable),color='k')
-    plt.title("regression fit:"+str(count))            
+    residual =  y - predictions
+    #residual qqplot
+    sm.qqplot(residual)
+    plt.title("QQ plot for single variable: X"+str(count))
+    plt.savefig("QQ plot of residual for linear regression of: X"+str(count)+" .png")
     plt.show()
     
-    print('Coeff of determination:'+str(count), lm.score(variable, y))
-    print('correlation is:'+str(count), math.sqrt(lm.score(variable, y)))
+
+    #residual scatter plot 
+    plt.scatter(predictions, residual,color='g')
+    plt.ylabel('residual');
+    plt.title("Residual scatter plot for single variable :"+str(count))
+    plt.xlabel('x - current_independent_variable')
+    plt.savefig("residual scatter for linear regression of: X"+str(count)+" .png")
+    plt.show()
+
+    #residual histogram
+
+    plt.hist(residual, bins=30)
+    plt.title("Histogram of residual:"+str(count))        
+    plt.savefig("residual histogram for linear regression of: X"+str(count)+" .png")
+    plt.show()
+
+    #Chi square test
+    print(stats.normaltest(residual))
+
+    #Plot regression fit using sklearn fitting again (remove this) 
+    lm=linear_model.LinearRegression()
+    lm.fit(current_independent_variable, y)
     
-#Task 3:
-#Multipvariable regression
 
-fig = plt.figure()
+    plt.scatter(current_independent_variable, y,color='g')
+    plt.plot(current_independent_variable, lm.predict(current_independent_variable),color='k')
+    plt.title("regression fit:"+str(count))            
+    plt.savefig("Regression fit for linear regression of: X"+str(count)+" .png")
+    plt.show()
+    
+    print('Coeff of determination:'+str(count), lm.score(current_independent_variable, y))
+    print('correlation is:'+str(count), math.sqrt(lm.score(current_independent_variable, y)))
 
-multi_variables = tuple(zip(x1, x2, x3, x4, x5))
+input()
+
+#Task 2: Polymnomial regression
+#fit for higher order polynomial:
+regression_variable = list(df.keys())
+multi_variables = list(zip(df[regression_variable[0]], [i**2 for i in df[regression_variable[0]]]))
 X = sm.add_constant(multi_variables) # adding a constant
      
 model = sm.OLS(y, X).fit()
 predictions = model.predict(X) 
 
-#residual = predictions.resid
+#residual calculation
 residual =  y - predictions 
-#plt.scatter(predictions, residual,color='g')
-#plt.show()
-sm.qqplot(residual, line='45')
+
+#residual scatter plot
+plt.scatter(predictions, residual,color='g')
+plt.title("Residual scatter plot X1:  y = a0 + a1*x1 + a2*(x1**2)")
+plt.savefig("Residual_scatter_plot_for_ploynomial.png")
 plt.show()
 
+#residual histogram
+plt.hist(residual, bins=30)
+plt.title("Histogram of residual for polynomial regression X1:  y = a0 + a1*x1 + a2*(x1**2)")        
+plt.savefig("residual histogram for polynomial regression of x1.png")
+plt.show()
+
+#Chi square test
+print(stats.normaltest(residual))
+
+
+print_model = model.summary()
+print(print_model)
+#regression fit plots
+fig = plt.figure(figsize=(20,12))
+fig = sm.graphics.plot_partregress_grid(model, fig=fig)
+plt.title("Residual scatter for polynomial regression plot X1:  y = a0 + a1*x1 + a2*(x1**2)")
+plt.savefig("Regression_fit_polynomial.png")
+fig.show()
+
+#qq plot of residuals
+sm.qqplot(residual)
+plt.title("QQplot for polynomial regression plot X1:  y = a0 + a1*x1 + a2*(x1**2)")
+plt.savefig("QQplot for polynomial.png")
+plt.show()
+input()
+
+#Task 3:
+#Multipvariable regression
+
+regression_variable = list(df.keys())
+multi_variables = tuple(zip(df[regression_variable[0]], df[regression_variable[1]], df[regression_variable[2]], df[regression_variable[3]], df[regression_variable[4]]))
+X = sm.add_constant(multi_variables) # adding a constant
+     
+model = sm.OLS(y, X).fit()
+predictions = model.predict(X) 
+
+#residual calculation and qqplot 
+residual =  y - predictions
+
+#residual scatter plot 
+plt.scatter(predictions, residual,color='g')
+plt.title("Residual scatter plot :  y = a0 + a1*x1 + a2*x2 + a3*x3 + a4*x4 + a5*x5")
+plt.savefig("Residual_scatter_plot_for_multi_variable.png")
+plt.show() 
+
+#residual histogram
+plt.hist(residual, bins=30)
+plt.title("Histogram of residual for polynomial regression :  y = a0 + a1*x1 + a2*x2 + a3*x3 + a4*x4 + a5*x5")        
+plt.savefig("residual histogram for multi_variable regression of x1.png")
+plt.show()
+
+#Chi square test
+print(stats.normaltest(residual))   
 
 print_model = model.summary()
 print(print_model)
 fig = plt.figure(figsize=(20,12))
 fig = sm.graphics.plot_partregress_grid(model, fig=fig)
+plt.title("Regression fit for multi variable regression plot :  y = a0 + a1*x1 + a2*x2 + a3*x3 + a4*x4 + a5*x5")
+plt.savefig("Regression_fit_multi_variable.png")
 fig.show()
 
+sm.qqplot(residual)
+plt.title("QQplot for multi variable regression plot :  y = a0 + a1*x1 + a2*x2 + a3*x3 + a4*x4 + a5*x5")
+plt.savefig("QQplot_for_multi_variable.png")
+plt.show()
+
 input()
+
+
 
